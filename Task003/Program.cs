0ce6ba7538c1de41e02158e63117d30a8b3b3907
Алексей Суслов <suslovwow@yandex.ru>
2023-10-22 17:34:23 +0300

    task003 done

---
+++ Task003/Program.cs
@@ -0,0 +1,29 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+int[] GenerateArray(int sizeArray, int leftRange, int rightRange)
+{
+    int[] newArray = new int[sizeArray];
+    Random rand = new Random();
+
+    for (int i = 0; i < newArray.Length; i++)
+    {
+        newArray[i] = rand.Next(leftRange, rightRange + 1);
+    }
+
+    return newArray;
+}
+
+void PrintArray(int[] arrayForPrint)
+{
+    for (int i = 0; i < arrayForPrint.Length; i++)
+    {
+        System.Console.Write(arrayForPrint[i] + " ");
+    }
+}
+
+// -------------------------------
+
+int[] array = GenerateArray(8, -10, 10);
+PrintArray(array);
